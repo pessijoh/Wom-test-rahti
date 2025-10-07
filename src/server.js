acac55b6a83261036aae9a2d0ce197211733db41
pessijoh <150067834+pessijoh@users.noreply.github.com>
2025-10-07 11:00:46 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 8080
+const FOO = process.env.FOO || "FOO not found in env"
 
 console.log(`Node.js ${process.version}`)
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({ msg: "Rahti2 node 0.2" })
+    res.json({ msg: "Rahti2 node 0.3" })
 })
 
 
